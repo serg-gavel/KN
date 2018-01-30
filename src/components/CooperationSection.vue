a5d25a2d2e7abfff2d079c245d9af7615e7fedee
serg-gavel <gavelyuk.sergey.13@gmail.com>
2018-01-30 19:49:06 +0200

    update repository

--- src/components/CooperationSection.vue
+++ src/components/CooperationSection.vue
@@ -4,10 +4,15 @@
       <img src="../../src/assets/img/cooperation.jpg" alt="cooperation-image">
     </div>
     <div class="cooperation-description">
-      <h3>Cooperation</h3>
+      <h3 class="cooperation-heading">{{ CooperationHeader }}</h3>
       <div class="description-text">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, cum?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda obcaecati
+          quas sed totam vitae! Ab, accusamus beatae culpa cum dolor doloremque dolores
+          eos error est et excepturi illum ipsa laborum molestias natus neque nesciunt nisi
+          nostrum odit quibusdam quis quos ratione recusandae rem rerum sapiente sunt tempore
+          temporibus voluptas voluptate.
         </p>
       </div>
       <ButtonBox>ButtonBox</ButtonBox>
@@ -22,18 +27,42 @@ export default {
   name: 'CooperationSection',
   data () {
     return {
-      msg: 'Cooperation Section!'
+      CooperationHeader: 'Сотрудничество'
     }
   },
   components: { ButtonBox }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
   .cooperation-section{
+    padding-top: 20px;
     order: 2;
     height: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     background: rgba(37, 130, 59, 0.68);
+    .cooperation-image{
+      flex-basis: 50%;
+      img{
+        width: 100%;
+      }
+    }
+    .cooperation-description{
+      flex-basis: 50%;
+      h3.cooperation-heading{
+        font: 1.5rem "Oswald";
+        padding-left: 8%;
+        text-align: start;
+        align-self: flex-start;
+      }
+      .description-text{
+        padding: 0 5%;
+        p{
+          text-align: start;
+        }
+      }
+    }
   }
 </style>
