5764b3e05c25917f373cba0ec0b374e75c7e0951
serg-gavel <gavelyuk.sergey.13@gmail.com>
2018-01-30 19:09:11 +0200

    update repository

---
+++ src/components/CooperationSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="cooperation-section">
+    <h1>{{msg}}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CooperationSection',
+  data () {
+    return {
+      msg: 'Cooperation Section!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .cooperation-section{
+    order: 2;
+    height: 100%;
+    background: rgba(37, 130, 59, 0.68);
+  }
+</style>
