43cdab9d453b1f2559ee2efab300ae936629e166
serg-gavel <gavelyuk.sergey.13@gmail.com>
2018-01-30 18:37:59 +0200

    update repository

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
       <AppMain>App Main</AppMain>
     </div>
     <AppFooter>App Footer</AppFooter>
-    <!--<img src="./assets/logo.png">-->
   </div>
 </template>
 
@@ -25,32 +24,15 @@ export default {
   /*main styles*/
 @include reset_css;
 @include decoration_reset;
-html{
-  /*height: 100%;*/
-}
-body{
-  /*display: flex;*/
-  /*flex-direction: column;*/
-  /*min-height: 100vh;*/
-  /*display: flex;*/
-  /*flex-direction: column;*/
-  /*min-height: 100vh;*/
-}
 #app {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
   background: #f3f3f3;
-  //flex prop
-  /*display: flex;*/
-  /*flex-direction: column;*/
-  //flex prop end
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   font-family: 'Oswald', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  /*color: #2c3e50;*/
 }
   .container{
     background: #fff;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 import MainSection from '@/components/MainSection'
 import BlogSection from '@/components/BlogSection'
 import ServicesSection from '@/components/ServicesSection'
