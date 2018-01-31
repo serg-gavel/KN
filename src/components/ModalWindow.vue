491480d6f5fa584bb0e627a94031e82eb778a2b2
serg-gavel <gavelyuk.sergey.13@gmail.com>
2018-01-31 20:03:42 +0200

    update repository

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="modal-window">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ModalWindow',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/main";
+  .modal-window{
+  }
+  /*media*/
+  @media (max-width: 1200px) {
+  }
+  @media (max-width: 998px) {
+  }
+  @media (max-width: 770px) {
+  }
+  @media (max-width: 570px) {
+  }
+  @media (max-width: 500px) {
+  }
+  @media (max-width: 430px) {
+  }
+  @media (max-width: 330px) {
+  }
+</style>
