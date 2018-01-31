8adf6bc88b2fc797e1fdab2ef2d2ef30e6957a41
serg-gavel <gavelyuk.sergey.13@gmail.com>
2018-01-31 22:20:26 +0200

    update repository

--- src/components/ButtonBox.vue
+++ src/components/ButtonBox.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="button-box">
     <router-link to="/blog" tag="button" class="visit-blog-button btn">Перейти в блог</router-link>
-    <button class="enroll-button btn">Записаться на маникюр</button>
-    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <button class="enroll-button btn" id="show-modal" @click="showModal = true">Записаться на маникюр</button>
       <ModalWindow v-if="showModal" @close="showModal = false">
         <h3 slot="header">custom header</h3>
+        <h3 slot="body">custom body</h3>
+        <h3 slot="footer">custom footer</h3>
       </ModalWindow>
   </div>
 </template>
@@ -43,6 +44,7 @@ export default {
     border: 2px solid $button-color;
     cursor: pointer;
     font-family: 'Oswald', sans-serif;
+    font-size: 0.9rem;
     line-height: 1.5;
     /*font: 700 1.2rem 'Roboto Slab', sans-serif;*/
     padding: 15px 25px;
@@ -67,6 +69,7 @@ export default {
 }
 @media (max-width: 500px) {
   .btn {
+    font-size: 0.8rem;
     margin: 5px 5px;
     padding: 10px 20px;
     @include common_animation;
